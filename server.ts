325b2f811521d690e01ea2cec0afcb1be0afbe2f
Kyle-Atienza <atienzakylefrancis@gmail.com>
2024-04-12 08:42:18 +0800

    init

---
+++ server.ts
@@ -0,0 +1,19 @@
+// src/index.js
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+const connectDB = require("./config/db");
+
+connectDB();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
